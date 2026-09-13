c8791e1b9cf0c3902d34867e7ab1d1c3752f1a79
Derrick Timmermans <derrick.timmermans@outlook.com>
2026-09-13 13:08:08 +0200

    Generate initramfs

---
+++ bootc/initramfs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+set -eoux pipefail
+
+KERNEL_VERSION=$(rpm -q --queryformat="%{evr}.%{arch}" kernel-core)
+
+export DRACUT_NO_XATTR=1
+/usr/bin/dracut --kver "${KERNEL_VERSION}" --reproducible -v -f "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
+chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
